8d4d9cc3e52501eeaaacc8826dc821b69c115520
david <davidparkerhl@gmail.com>
2019-06-04 21:40:54 +0800

    asdf

--- Main.java
+++ Main.java
@@ -116,7 +116,7 @@ public class Main {
 
 ///////////     表单展示设置
         // 设置表格内容颜色
-/*
+
         table.setForeground(Color.BLACK);                   // 字体颜色
         table.setFont(new Font(null, Font.PLAIN, 14));      // 字体样式
         table.setSelectionForeground(Color.BLACK);      // 选中后字体颜色
@@ -134,7 +134,6 @@ public class Main {
 
         // 第一列列宽设置为40
         table.getColumnModel().getColumn(0).setPreferredWidth(40);
-*/
         // 设置滚动面板视口大小（超过该大小的行数据，需要拖动滚动条才能看到）
         table.setPreferredScrollableViewportSize(new Dimension(980, 400));
 
@@ -146,102 +145,99 @@ public class Main {
         // 创建内容面板
         JPanel panel = new JPanel();
 
-        JTextField filterText = new JTextField("",10);
-        panel.add(new JLabel("搜索内容："));
-        panel.add(filterText);
 
-        JButton searchButton = new JButton("搜索");
-        searchButton.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
-                String text = filterText.getText();
-                if (text.length() == 0)
+        // 名字 种类 颜色 年龄 性别 服务 状态 主人 是否会员
+                JTextField[] tf=new JTextField[colNum];
+                final int WIDTH=3;
+                for(int i=0;i<colNum;i++)
                 {
-                    sorter.setRowFilter(null);
+                  panel.add(new JLabel(columnNames[i]));
+                  tf[i]=new JTextField("",WIDTH);
+                  panel.add(tf[i]);
                 }
-                else
+
+                final JButton addButton = new JButton("添加");   //添加按钮
+                addButton.addActionListener(new ActionListener()
+                {//添加事件
+                    public void actionPerformed(ActionEvent e)
+                    {
+                        String[] newData = new String[colNum];
+                        for(int i=0;i<colNum;i++)
+                          newData[i]=tf[i].getText();
+                        tableModel.addRow(newData);  //添加一行
+                        // int rowCount = table.getRowCount() +1;   //行数加上1
+                    }
+                });
+                panel.add(addButton);
+
+                final JButton delButton = new JButton("删除");
+                delButton.addActionListener(new ActionListener()
                 {
-                    sorter.setRowFilter(RowFilter.regexFilter(text));
-                }
-            }
-        });
-        panel.add(searchButton);
+                    public void actionPerformed(ActionEvent e)
+                    {
+                        int selectedRow = table.getSelectedRow();//获得选中行的索引
+                        if(selectedRow!=-1)  //存在选中行
+                        {
+                            tableModel.removeRow(selectedRow);  //删除行
+                        }
+                    }
+                });
+                panel.add(delButton);
+
+
+
 
         // 添加 数据部分滚动面板 到 内容面板
         JScrollPane scrollPane = new JScrollPane(table);
         panel.add(scrollPane);
 
-// 名字 种类 颜色 年龄 性别 服务 状态 主人 是否会员
-        final int WIDTH=3;
-        panel.add(new JLabel("名字: "));
-        JTextField nameField = new JTextField("",WIDTH);
-        panel.add(nameField);
+        JTextField filterText = new JTextField("",10);
+        panel.add(new JLabel("搜索内容："));
+        panel.add(filterText);
 
-        panel.add(new JLabel("种类:"));
-        JTextField typeField = new JTextField("",WIDTH);
-        panel.add(typeField);
-
-        panel.add(new JLabel("颜色:"));
-        JTextField colourField = new JTextField("",WIDTH);
-        panel.add(colourField);
-
-        panel.add(new JLabel("年龄:"));
-        JTextField ageField = new JTextField("",WIDTH);
-        panel.add(ageField);
-
-        panel.add(new JLabel("性别:"));
-        JTextField genderField = new JTextField("",WIDTH);
-        panel.add(genderField);
-
-        panel.add(new JLabel("服务:"));
-        JTextField serviceField = new JTextField("",WIDTH);
-        panel.add(serviceField);
-
-        panel.add(new JLabel("状态:"));
-        JTextField statusField = new JTextField("",WIDTH);
-        panel.add(statusField);
-
-        panel.add(new JLabel("主人:"));
-        JTextField ownerField = new JTextField("",WIDTH);
-        panel.add(ownerField);
-
-        panel.add(new JLabel("是否会员:"));
-        JTextField premiumField = new JTextField("",WIDTH);
-        panel.add(premiumField);
-
-        final JButton addButton = new JButton("添加");   //添加按钮
-        addButton.addActionListener(new ActionListener()
-        {//添加事件
-            public void actionPerformed(ActionEvent e)
-            {
-                String[] rowValues = {nameField.getText(),typeField.getText(),colourField.getText(),ageField.getText(),genderField.getText(),serviceField.getText(),statusField.getText(),ownerField.getText(),premiumField.getText()};
-                tableModel.addRow(rowValues);  //添加一行
-                // int rowCount = table.getRowCount() +1;   //行数加上1
-            }
-        });
-        panel.add(addButton);
-
-        final JButton delButton = new JButton("删除");
-        delButton.addActionListener(new ActionListener()
-        {//添加事件
-            public void actionPerformed(ActionEvent e)
-            {
-                int selectedRow = table.getSelectedRow();//获得选中行的索引
-                if(selectedRow!=-1)  //存在选中行
+                JButton searchButton = new JButton("搜索");
+                searchButton.addActionListener(new ActionListener()
                 {
-                    tableModel.removeRow(selectedRow);  //删除行
-                }
-            }
-        });
-        panel.add(delButton);
+                    public void actionPerformed(ActionEvent e)
+                    {
+                        String text = filterText.getText();
+                        if (text.length() == 0)
+                        {
+                            sorter.setRowFilter(null);
+                        }
+                        else
+                        {
+                            sorter.setRowFilter(RowFilter.regexFilter(text));
+                        }
+                    }
+                });
+                panel.add(searchButton);
 
+                JButton cancelButton = new JButton("取消");
+                cancelButton.addActionListener(new ActionListener()
+                {
+                    public void actionPerformed(ActionEvent e)
+                    {
+                        sorter.setRowFilter(null);
+                    }
+                });
+                panel.add(cancelButton);
 
 
 
 /////////   窗口处理
         // 设置 内容面板 到 窗口
         SFrame jf = new SFrame(table,columnNames);
+
+
+        // jf.getContentPane()).setOpaque(false);
+    	ImageIcon img = new ImageIcon("/home/david/Desktop/2.jpg");
+    	JLabel background = new JLabel(img);
+    	jf.getLayeredPane().add(background, new Integer(Integer.MIN_VALUE));
+      jf.getContentPane().setVisible(false);
+    	background.setBounds(0, 0, img.getIconWidth(), img.getIconHeight());
+
+
         jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         jf.setContentPane(panel);
         jf.pack();
@@ -252,3 +248,5 @@ public class Main {
 
     }
 }
+
+//名字 种类 颜色 年龄 性别 服务 状态 主人 是否会员
